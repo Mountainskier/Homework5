8d6093788779957f9e8d87431d902322ff59f530
Владимир <169135025+Mountainskier@users.noreply.github.com>
2024-05-14 16:10:05 +0300

    Add files via upload

---
+++ Homework5.py
@@ -0,0 +1,16 @@
+my_list = ['orange', 'apple', 'kiwi', 'lemon', 'cherry', 'peach', 'mandarin', 'lime', 'pear']
+print(my_list)
+print(my_list[0])
+print(my_list[-1])
+print(my_list[2:5])
+my_list[2] = 'coconut'
+print(my_list)
+
+
+my_dict = {'phone': 'телефон', ' guitar': 'гитара', ' water': 'вода', 'mouse': 'мышь'}
+print(my_dict)
+print(my_dict[' guitar'])
+my_dict['table'] = 'Стол'
+print(my_dict)
+
+
